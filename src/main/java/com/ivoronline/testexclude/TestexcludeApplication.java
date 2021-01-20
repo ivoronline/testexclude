3510cb6d2ac6a339ae5155b2da2a5b0d81c2296f
igoreta <ivoronline@gmail.com>
2021-01-20 11:25:20 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/testexclude/TestexcludeApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.testexclude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestexcludeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestexcludeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/testexclude/TestexcludeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.testexclude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestexcludeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
